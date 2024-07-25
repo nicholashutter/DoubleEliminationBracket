1e3c7df8b068ba2d98076ba37c8fa668abecccbf
nicholas hutter <nicholas.hutter@live.com>
2024-07-25 10:03:36 -0500

    Removing bootstrap bloat from generate-express-ts cleaning up codebase

--- expressServer/src/server.ts
+++ expressServer/src/server.ts
@@ -6,14 +6,12 @@ import cookieParser from "cookie-parser";
 import morgan from "morgan";
 import path from "path";
 import helmet from "helmet";
-import express, { Request, Response, NextFunction } from "express";
+import express, { Request, Response, NextFunction} from "express";
 import logger from "jet-logger";
 
 import "express-async-errors";
 
-import BaseRouter from "./routes";
 
-import Paths from "./common/Paths";
 import EnvVars from "./common/EnvVars";
 import HttpStatusCodes from "./common/HttpStatusCodes";
 import RouteError from "./common/RouteError";
@@ -47,33 +45,28 @@ if (EnvVars.NodeEnv === NodeEnvs.Production.valueOf()) {
 }
 
 // Add APIs, must be after middleware
-app.use(Paths.Base, BaseRouter);
+//app.use(Paths.Base);
 
 // Add error handler
-app.use(
-  (
-    err: Error,
-    _: Request,
-    res: Response,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    next: NextFunction
-  ) => {
-    if (EnvVars.NodeEnv !== NodeEnvs.Test.valueOf()) {
-      logger.err(err, true);
-    }
-    let status = HttpStatusCodes.BAD_REQUEST;
-    if (err instanceof RouteError) {
-      status = err.status;
-    }
-    return res.status(status).json({ error: err.message });
+const errorMiddleware = (
+  err: Error,
+  req: express.Request,
+  res: express.Response,
+  next: NextFunction
+) => {
+  if (EnvVars.NodeEnv !== NodeEnvs.Test.valueOf()) {
+    logger.err(err, true);
   }
-);
+  let status = HttpStatusCodes.BAD_REQUEST;
+  return res.status(status).json({ error: 'Error occured in middleware'});
+};
+
+app.use(errorMiddleware);
+
+
 
 // **** Front-End Content **** //
 
-// Set views directory (html)
-const viewsDir = path.join(__dirname, "views");
-app.set("views", viewsDir);
 
 // Set static directory (js and css). //I have set this to build directory for react allowing the index.html and index.js to be the entrypoints from here
 //reactrouter should take over once this page is served
