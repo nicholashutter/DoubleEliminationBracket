f34967525441154adbcb53d4a5ef38d310e65e4f
nicholas hutter <nicholas.hutter@live.com>
2024-09-06 09:03:50 -0500

    unit testing now works

--- expressServer/jest.config.ts
+++ expressServer/jest.config.ts
@@ -77,17 +77,17 @@ const config: Config = {
   //   "node_modules"
   // ],
 
-  // An array of file extensions your modules use
-  // moduleFileExtensions: [
-  //   "js",
-  //   "mjs",
-  //   "cjs",
-  //   "jsx",
-  //   "ts",
-  //   "tsx",
-  //   "json",
+  
+  moduleFileExtensions: [
+    "js",
+    "mjs",
+    "cjs",
+    "jsx",
+    "ts",
+    "tsx",
+    "json",
   //   "node"
-  // ],
+     ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
@@ -155,10 +155,10 @@ const config: Config = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  // testMatch: [
-  //   "**/__tests__/**/*.[jt]s?(x)",
-  //   "**/?(*.)+(spec|test).[tj]s?(x)"
-  // ],
+  testMatch: [
+  "**/__tests__/**/*.[jt]s?(x)",
+  "**/?(*.)+(spec|test).[tj]s?(x)"
+  ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
@@ -194,6 +194,7 @@ const config: Config = {
 
   // Whether to use watchman for file crawling
   // watchman: true,
+  preset: "ts-jest"
 };
 
 export default config;
